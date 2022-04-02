fbf2a37edc3b45af4e9892bbf8033a9e036f53d8
KirillKurko <kirillkurko2609@gmail.com>
2022-04-02 03:45:04 +0300

    Basic implementation

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+import React from 'react';
+import FilterLink from '../containers/FilterLink';
+import { VisibilityFilters } from '../actions';
+
+function Footer() {
+  return (
+    <div>
+      <span>Show: </span>
+      <FilterLink filter={VisibilityFilters.SHOW_ALL}>All</FilterLink>
+      <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>Active</FilterLink>
+      <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>
+        Completed
+      </FilterLink>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/containers/AddTodo.js
@@ -0,0 +1,28 @@
+/* eslint-disable */
+import React from 'react';
+import { connect } from 'react-redux';
+import { addTodo } from '../actions';
+
+function AddTodo({ dispatch }) {
+  let input;
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (!input.value.trim()) {
+            return;
+          }
+          dispatch(addTodo(input.value));
+          input.value = '';
+        }}
+      >
+        <input ref={(node) => (input = node)} />
+        <button type='submit'>Add Todo</button>
+      </form>
+    </div>
+  );
+}
+
+export default connect()(AddTodo);
